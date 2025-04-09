5cb6be4dbb7954e46a0f6208d8ce7c8d750263ef
Jahnavi0485 <2210030485@klh.edu.in>
2025-04-09 10:38:50 +0530

    jah

---
+++ programs/demo1.js
@@ -0,0 +1,6 @@
+import {addNumber, subtractNumber, multiplyNumber, divideNumber} from './demo.js'
+
+console.log(addNumber(1006,3))
+console.log(subtractNumber(8907,3))
+console.log(multiplyNumber(5678,3))
+console.log(divideNumber(2334,3))
